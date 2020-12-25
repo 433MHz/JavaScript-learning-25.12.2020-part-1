cc08af53382c3cc08cb0b25ab6d94b7cbf41391d
Krystian <user@DESKTOP-HNKK2FJ>
2020-12-25 12:34:18 +0100

    corrected wrong time format on page

--- timer.js
+++ timer.js
@@ -7,6 +7,24 @@ function timer(){
 			var minuta = data.getMinutes();
 			var sekunda = data.getSeconds();
 
+			if(godzina < 10){
+				godzina = "0" + godzina;
+			}
+			if(minuta < 10){
+				minuta = "0" + minuta;
+			}
+			if(sekunda < 10){
+				sekunda = "0" + sekunda;
+			}
+			if(dzien < 10){
+				dzien = "0" + dzien;
+			}
+			if(miesiac < 10){
+				miesiac = "0" + miesiac;
+			}
+			
+
+
 			var czas = "Godzina: " + godzina + ":" + minuta + ":" + sekunda;
 			var czas2 = "Miesiac: " + dzien + ":" + miesiac + ":" + rok;
 
